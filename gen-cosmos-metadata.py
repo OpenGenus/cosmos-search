fecd5dfaed14660ad793286061304785f99daddb
Kapil Lamba <kapillamba4@gmail.com>
2017-11-08 00:41:42 +0530

    Add api support for cosmos metadata json generator

--- gen-cosmos-metadata.py
+++ gen-cosmos-metadata.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 """A python script to generate the meta-data of the cosmos repository for search tool"""
-from os import walk
 import json
+import requests
+import sys
+
+base_url = "https://api.github.com/repos/OpenGenus/cosmos/contents/"
+token = "YOUR_TOKEN_HERE"
+result = {}
+
+def traverse(path):
+    r = requests.get(base_url+path+"?access_token="+token)
+    data = r.json()
+    if "message" in data and data["message"] == "Bad credentials":
+        print("Bad credentials/Api Rate Limit exceeded")
+        sys.exit(0)
+
+    files = []
+    for dirname in data:
+        if dirname["type"] == "file":
+            files.append(dirname["name"])
+            print(dirname["name"])
+        else:
+            traverse(dirname["path"])
+
+    if len(files) > 0:
+        result[path] = files
 
 if __name__ == "__main__":
-    data = {}
-    for (dirpath, dirnames, filenames) in walk('code/'):
-        if dirnames == []:
-            data[dirpath] = filenames
-    with open('metadata.json', 'w') as f:
-        json.dump(data, f)
+    traverse("code")
+    with open("metadata.json", "w") as f:
+        json.dump(result, f)      
