7fb0dfb4be3d32965afd99ece1cedaa77460e1e4
Sakshee-19 <jocund.isha@gmail.com>
2018-03-07 20:16:41 +0530

    add feat: computational power

--- cosmos_search/urls.py
+++ cosmos_search/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls import handler400
 from django.conf.urls import handler403
 from django.conf.urls import handler404
 from django.conf.urls import handler500
+from django.views.static import serve
+
+from cosmos_search import settings
 from search import views
 
 urlpatterns = [
@@ -26,6 +30,13 @@ urlpatterns = [
     url(r'^update/', include('update.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG is False:
+    urlpatterns += [url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}), ]
+
 
 handler400 = views.error400  # noqa
 handler403 = views.error403  # noqa
