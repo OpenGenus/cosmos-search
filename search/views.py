90712e2482bcdbd7f25e404025afd5849b23573b
Sarthika Dhawan <sarthika15170@iiitd.ac.in>
2018-03-11 19:08:55 +0530

    Update views.py

--- search/views.py
+++ search/views.py
@@ -1,35 +1,68 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
 import json
 import random
 from random import shuffle
-import math
+import re
+
+COSMOS_SEP = '_'
+
+
 # Create your views here
 
-# To prefill the searchbar
-def searchbar():
-    jsonFile = open('tags.json', 'r')
-    data = json.load(jsonFile)
-    algo_list = data['tags']
-    r_no = random.randint(0,len(algo_list))
-    algo_tag = algo_list[r_no]
 
+# To prefill the searchbar
+def get_random_tag():
+    jsonFile = open(settings.TAGS_JSON, 'r')
+    algo_list = json.load(jsonFile)
+    r_no = random.randint(0, len(algo_list) - 1)
+    algo_tag = algo_list[r_no]
     return algo_tag,algo_list
 
-    
+
+def searchSuggestion(request):
+    jsonFile = open(settings.TAGS_JSON, 'r')
+    algo_list = json.load(jsonFile)
+    filterData = []
+    results = []
+    if request.is_ajax():
+        val = request.GET.get('term', '')
+        filterData = []
+        for word in algo_list:
+            if word.startswith(val):
+                filterData.append(word)
+        i = 0
+        for tag in filterData:
+            tag_json = {}
+            tag_json['id'] = filterData.index(tag)
+            tag_json['label'] = tag
+            tag_json['value'] = tag
+            results.append(tag_json)
+            i = i + 1
+            if i >= 6:
+                break
+        searchTag = json.dumps(results)
+    else:
+        searchTag = 'fail'
+    return searchTag
+
 
 def index(request):
-    algo_tag,algo_list = searchbar()
+    algo_tag,algo_list = get_random_tag()
+    algo = searchSuggestion(request)
     algo_list_query=[]
     for a in algo_list:
         s=a.split(" ")
         ss='+'.join(s)
         algo_list_query.append(ss)
 
+    if request.is_ajax():
+        mimetype = 'application/json'
+        return HttpResponse(algo, mimetype)
     tabbed_algos_shortlist=zip(algo_list[:10],algo_list_query[:10])
     tabbed_algos=zip(algo_list[10:],algo_list_query[10:])
     return render(request,'cosmos/index.html',{'algo_name':algo_tag,'tabbed_algos':tabbed_algos,'tabbed_algos_shortlist':tabbed_algos_shortlist})
-
 
 
 # Handlers for error pages
@@ -49,50 +82,67 @@ def error500(request):
     return render(request, 'cosmos/error/HTTP500.html')
 
 
+def is_file_extension_ignored(file_):
+    return file_.split('.')[-1] in ['md', 'MD']
+
 
 # Search query
 def query(request):
-    query = request.GET['q']
-    q = query.replace(' ', '_')
-
-
+    query = re.escape(request.GET['q']).replace('\ ', ' ')
+    q = query.replace(' ', COSMOS_SEP)
     data = json.loads(open(settings.METADATA_JSON, 'r').readline())
     ans = []
     rec = []
-
-    for k, v in data.items():
+    amount = 0
+    for folder, file in data.items():
         filtered_v = []
-        try:
-            for f in v:
-                if f.split('.')[-1] != 'md':
-                    filtered_v.append(f)
-        except TypeError:
-            print('TypeError')
-        
-        # We do not want /test/ in the results if the user types in "test" in the searchbox
-        if q in k:
+        for f in file:
+            if not is_file_extension_ignored(f):
+                filtered_v.append(f)
+        if q in folder and "test" not in folder.split("/"):
             if filtered_v:
-                path = k
-                k = k.split('/')
-                ans.append({'path': path, 'dirs': k, 'files': filtered_v})
-                if len(k) == 2:
-                    d = k[len(k)-2] + '/'
+                path = folder
+                folder_list = folder.split('/')
+                ans.append({'path': path, 'dirs': folder_list, 'files': filtered_v})
+                amount += len(filtered_v)
+                if len(folder_list) == 2:
+                    d = folder_list[-2] + '/'
                 else:
-                    d = k[len(k)-3] + '/'
+                    d = folder_list[-3] + '/'
                 for i, j in data.items():
-                        if d in i:
-                            if not q in i:
-                                p = i
-                                p = p.split('/')
-                                l = p[len(p)-1]
-                                rec.append({'recpath': i, 'recdirs':p, 'last': l})
+                    if d in i:
+                        if q not in i:
+
+                            only_contents_md = True
+                            for f in j:
+                                if not is_file_extension_ignored(f):
+                                    only_contents_md = False
+                                    break
+                            if only_contents_md:
+                                continue
+
+                            p = i
+                            p = p.split('/')
+                            l = p[-1]
+                            rec.append({'recpath': i, 'recdirs': p, 'last': l})
     if not ans:
         return render(request, 'cosmos/notfound.html', {'query': query})
     shuffle(rec)
-    return render(request, 'cosmos/searchresults.html',
-                  {'amount': len(ans), 'result': ans, 'recommend': rec[0:5], 'query': query})
+    if request.is_ajax():
+        algo = searchSuggestion(request)
+        mimetype = 'application/json'
+        return HttpResponse(algo, mimetype)
+    else:
+        return render(request, 'cosmos/searchresults.html',
+                      {'amount': amount,
+                       'result': ans,
+                       'recommend': rec[:5],
+                       'query': query,
+                       'algo_name': query
+                       })
 
-# search strategy
+
+# Search strategy
 def subsq(a, b, m, n):
     # Base Cases
     if m == 0:
