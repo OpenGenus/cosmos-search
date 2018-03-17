1faea38204ebac74b0e6fae399369559195e3b1b
sarvanideekshitula <sarvanimini@gmail.com>
2018-03-17 19:55:41 +0530

    Changes

--- cosmos_search/settings.py
+++ cosmos_search/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
