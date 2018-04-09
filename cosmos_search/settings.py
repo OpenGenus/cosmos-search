af2779e4d2b8330641f90a5b8922378d49d0401b
muskan6769 <iit2017046@iiita.ac.in>
2018-04-09 22:03:04 +0530

    removed secret key

--- cosmos_search/settings.py
+++ cosmos_search/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY='kVgmrbouj0HCkShAtwLwIg0u'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
