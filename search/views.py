f836c5d3cdb047d6aa491c6a71bd51714bdda1e8
khatryshikha <shikhakhatry2612@gmail.com>
2018-04-17 00:50:20 +0530

    multiple results at once

--- search/views.py
+++ search/views.py
@@ -13,7 +13,9 @@ COSMOS_SEP = '_'
 
 # Create your views here
 
-
+a=[]
+b=[]
+count=0
 # To prefill the searchbar
 def get_random_tag():
     jsonFile = open(settings.TAGS_JSON, 'r')
@@ -114,6 +116,7 @@ rec = []
 
 def query(request):
     global ans, rec
+    global a,b,count
     title = ''
     algo_name = ''
     amount = 0
@@ -167,6 +170,11 @@ def query(request):
                     path = folder
                     folder_list = folder.split('/')
                     ans.append({'path': path, 'dirs': folder_list, 'files': filtered_v})
+
+                    a.append(folder_list)
+                    b.append(filtered_v)
+                    count=count+1
+
                     amount += len(filtered_v)
                     if len(folder_list) == 2:
                         d = folder_list[-2] + '/'
@@ -287,14 +295,43 @@ def subsq(a, b, m, n):
     # If last characters are not matching
     return subsq(a, b, m, n - 1)
 
-
 def display(request):
     path = request.GET['path']
+    ans=[]
+    abspath = path
+    q=get_array1()
+    p=get_array2()
+    n=cnt()
+    new = path.split("/")
+    no=len(new)-2
+    for i in range(0,n):
+        num=len(q[i])
+        for j in range(0,num):
+            if new[no]==q[i][j]:
+                ans=p[i]
+                v=q[i]
+                s="/".join(v)  # path created
     display = "https://raw.githubusercontent.com/OpenGenus/cosmos/master/code/" + path
     r = requests.get(display)
+    
+    k = path.split('/')
+    nk = len(k)
+    a = k[0]
+    for i in range(1,nk-1):
+        a = a + '/' + k[i]
+    
     path = path.replace('_', ' ')
     path = path.replace('.', ' in ')
     l = path.split('/')
     if 'src' in l:
         l.remove('src')
-    return render(request, 'cosmos/data.html', {'code': r.text, 'path': l})
+    return render(request, 'cosmos/data.html', {'code': r.text, 'path': l,"result":ans, "apath": a})
+
+def get_array1():
+    return (a)
+
+def get_array2():
+    return(b)
+
+def cnt():
+    return(count)
