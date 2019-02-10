eb17e545c09ef190f3e3750ea977781b8c2e27bf
Ernest <iattempt.net@gmail.com>
2019-02-10 15:11:50 +0800

    Move files, Squashed commit of the following:
    commit 9a244fc1fcb42f71aacfabcb32dff38dc4948772
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sun Feb 10 11:48:58 2019 +0800
    
        Move CSS
    
    commit 39e373bd7b779e2579a0d62d3e0820e57f64dcd9
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sun Feb 10 11:48:58 2019 +0800
    
        Move templates
    
        Move templates
    
    commit 5e0ec909d3573a3a2f67e8d5019541e7b5a7d9cd
    Author: Ernest <iattempt.net@gmail.com>
    Date:   Sun Feb 10 11:41:59 2019 +0800
    
        Move images

--- cosmos_search/settings.py
+++ cosmos_search/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'cosmos_search.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
